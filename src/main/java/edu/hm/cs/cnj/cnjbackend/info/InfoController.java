1aa7d0e1113481a75702faefdd49bd890dae41b2
Huber <hm-huber98@hm.edu>
2017-11-07 14:36:24 +0100

    test33

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,15 +1,19 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InfoController {
+
+	@Autowired
+	InfoProperties properties;
 	
 	@RequestMapping("/info")
 	public Info greeting() {
 		
 		Info result = new Info();
-		result.setMessage("OK");
+		result.setMessage(properties.getMessage());
 		return result;
 	}
 }
